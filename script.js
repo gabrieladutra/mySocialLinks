ed539a54e811c55138e69b4814e2fb67e3478f27
gabrieladutra <gabriela_ddutra@outlook.com>
2023-01-18 16:12:51 -0300

    Added toggle profile picture

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function toggleMode() {
     const html = document.documentElement;
+    const img = document.querySelector("#profile img")
 
-    // if (html.classList.contains('light')) {
-    //     html.classList.remove('light')
-    // } else {
-    //     html.classList.add('light')
-    // }
     html.classList.toggle("light");
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('alt', 'Foto de Gabriela Dias Dutra sorrindo, usando uma camisa amarela')
+    } else
+        img.setAttribute('src', './assets/avatar.png')
 
+    img.setAttribute('alt', 'Foto de Gabriela Dias Dutra usando uma camisa branca')
 }
+
+//     html.classList.add('light')
+// }
+
+//pegar tag image
+//substituir imagem
